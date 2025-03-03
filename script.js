5319e2ed80543fbb1eb3ede8ffa177d2b3c3851f
TranDat-WD <trietdat318@gmail.com>
2025-03-03 21:52:31 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,129 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const audio = document.getElementById("background-audio");
+  audio.volume = 0.15;
+  const overlay = document.getElementById("overlay");
+
+  const video = document.getElementById("video");
+  video.addEventListener("ended", () => {
+    overlay.style.transition = "background-color 5s linear";
+
+    video.style.transition = "opacity 0.5s";
+    video.style.opacity = 0;
+
+    setTimeout(() => {
+      video.src = "assets/videos/Videos-" + getRandomInt(1, 30) + ".mp4";
+      video.load();
+      video.play();
+    }, 750);
+
+    setTimeout(() => {
+      video.style.opacity = 1;
+    }, 750);
+  });
+
+  // const lastVisit = localStorage.getItem("visitedToday");
+  // const today = new Date().toDateString();
+
+  // if (lastVisit === today) {
+  //   window.location.href = "quote-expl.html";
+  // } else {
+  //   localStorage.setItem("visitedToday", today);
+  // }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const audio = document.getElementById("audio");
+});
+
+function showCards() {
+  const cardsContainer = document.getElementById("cards-container");
+  const card1 = document.getElementById("card-1");
+  const card2 = document.getElementById("card-2");
+  const card3 = document.getElementById("card-3");
+  const cardAll = document.querySelectorAll(".cards-button");
+
+  cardsContainer.style.opacity = 1;
+  cardsContainer.style.pointerEvents = "all";
+  setTimeout(() => {
+    card1.style.opacity = 1;
+    card1.style.transform = "translateY(0)";
+  }, 1000);
+  setTimeout(() => {
+    card2.style.opacity = 1;
+    card2.style.transform = "translateY(0)";
+  }, 2000);
+  setTimeout(() => {
+    card3.style.opacity = 1;
+    card3.style.transform = "translateY(0)";
+    cardAll.forEach((card) => {
+      card.style.pointerEvents = "all";
+    });
+  }, 3000);
+}
+
+function theOtherTwo(number) {
+  switch (number) {
+    case 1:
+      return [2, 3];
+      break;
+    case 2:
+      return [1, 3];
+      break;
+    case 3:
+      return [1, 2];
+      break;
+  }
+}
+
+function resetCards() {
+  localStorage.setItem("avatar", getRandomInt(0, 1));
+  window.location.href = "quote-expl.html";
+}
+
+function flipCard(id) {
+  if (document.getElementById("card-" + id).classList.contains("flipped")) {
+    resetCards();
+    return;
+  }
+  const a = getRandomInt(1, 6);
+  localStorage.setItem("Topic", a);
+  localStorage.setItem("Quote", getRandomInt(0, 14));
+  const otherTwo = theOtherTwo(id);
+  switch (a) {
+    case 1:
+      document.getElementById("card-back-" + id).src =
+        "assets/cards/Thiên nga.png";
+      break;
+    case 2:
+      document.getElementById("card-back-" + id).src =
+        "assets/cards/Thiên hậu.png";
+      break;
+    case 3:
+      document.getElementById("card-back-" + id).src = "assets/cards/Sư tử.png";
+      break;
+    case 4:
+      document.getElementById("card-back-" + id).src =
+        "assets/cards/Đại hùng.png";
+      break;
+    case 5:
+      document.getElementById("card-back-" + id).src =
+        "assets/cards/Lạp hộ.png";
+      break;
+    case 6:
+      document.getElementById("card-back-" + id).src =
+        "assets/cards/Ma kết.png";
+      break;
+  }
+  document.getElementById("card-" + id).classList.toggle("flipped");
+
+  setTimeout(() => {
+    document.getElementById("card-" + otherTwo[0]).style.opacity = 0;
+    document.getElementById("card-" + otherTwo[0]).style.pointerEvents = "none";
+    document.getElementById("card-" + otherTwo[1]).style.opacity = 0;
+    document.getElementById("card-" + otherTwo[1]).style.pointerEvents = "none";
+  }, 100);
+}
---
+++ script1.js
@@ -0,0 +1,211 @@
+const avatar = ["assets/avatar/HH-Ava.jpg", "assets/avatar/TD-Ava.png"];
+
+/* Quote */
+
+const loveQuotes = [
+  "Love is not about how many days, months, or years you have been together. It's all about how much you love each other every single day.",
+  "The best thing to hold onto in life is each other.",
+  "Love is when the other person's happiness is more important than your own.",
+  "To love and be loved is to feel the sun from both sides.",
+  "A true love story never ends.",
+  "Love is not finding someone to live with, it’s finding someone you can’t live without.",
+  "You don't marry someone you can live with; you marry someone you cannot live without.",
+  "Love is not about possession, it’s about appreciation.",
+  "A great relationship doesn’t happen because of the love you had in the beginning, but how well you continue building love until the end.",
+  "Love is composed of a single soul inhabiting two bodies.",
+  "Love is not something you look for. Love is something you become.",
+  "The greatest happiness of life is the conviction that we are loved.",
+  "True love is rare, and it's the only thing that gives life real meaning.",
+  "You don’t love someone for their looks or their clothes, but because they sing a song only your heart can understand.",
+  "The best proof of love is trust.",
+];
+
+const learningQuotes = [
+  "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+  "An investment in knowledge pays the best interest.",
+  "The beautiful thing about learning is that no one can take it away from you.",
+  "Education is the passport to the future, for tomorrow belongs to those who prepare for it today.",
+  "The capacity to learn is a gift; the ability to learn is a skill; the willingness to learn is a choice.",
+  "A person who never made a mistake never tried anything new.",
+  "Learning never exhausts the mind.",
+  "The mind is not a vessel to be filled but a fire to be kindled.",
+  "Tell me and I forget, teach me and I may remember, involve me and I learn.",
+  "Knowledge is power.",
+  "A journey of a thousand miles begins with a single step.",
+  "You are always a student, never a master. You have to keep moving forward.",
+  "Every day is a new opportunity to learn and grow.",
+  "The only true wisdom is in knowing you know nothing.",
+  "Never stop learning, because life never stops teaching.",
+];
+
+const motivationQuotes = [
+  "Don’t watch the clock; do what it does. Keep going.",
+  "Opportunities don't happen. You create them.",
+  "Success is not the key to happiness. Happiness is the key to success.",
+  "Hardships often prepare ordinary people for an extraordinary destiny.",
+  "You miss 100% of the shots you don’t take.",
+  "The only way to do great work is to love what you do.",
+  "Your limitation—it’s only your imagination.",
+  "Do what you can, with what you have, where you are.",
+  "Dream big and dare to fail.",
+  "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
+  "Believe you can and you're halfway there.",
+  "Be stronger than your excuses.",
+  "Don't wait for opportunity. Create it.",
+  "Every accomplishment starts with the decision to try.",
+  "The best way to predict the future is to create it.",
+];
+
+const workQuotes = [
+  "Choose a job you love, and you will never have to work a day in your life.",
+  "Success is the sum of small efforts, repeated day in and day out.",
+  "Pleasure in the job puts perfection in the work.",
+  "The only way to do great work is to love what you do.",
+  "Work hard in silence, let success make the noise.",
+  "Don’t stop when you’re tired. Stop when you’re done.",
+  "Do more than is required. What is the distance between someone who achieves their goals consistently and those who spend their lives merely following? The extra mile.",
+  "It’s not about having time. It’s about making time.",
+  "If you want to achieve greatness, stop asking for permission.",
+  "Dreams don’t work unless you do.",
+  "The harder you work for something, the greater you’ll feel when you achieve it.",
+  "Don’t count the days, make the days count.",
+  "You were born to be real, not perfect.",
+  "Go as far as you can see; when you get there, you’ll be able to see further.",
+  "Opportunities are usually disguised as hard work, so most people don’t recognize them.",
+];
+
+const happinessQuotes = [
+  "Happiness is not something ready made. It comes from your own actions.",
+  "The purpose of our lives is to be happy.",
+  "Happiness depends upon ourselves.",
+  "The secret of happiness is to find joy in the simplest of things.",
+  "Happiness is the only thing that multiplies when you share it.",
+  "Do what makes your soul shine.",
+  "Happiness is not by chance, but by choice.",
+  "Happiness is when what you think, what you say, and what you do are in harmony.",
+  "For every minute you are angry, you lose sixty seconds of happiness.",
+  "Happiness is not a goal; it is a by-product of a life well-lived.",
+  "Life is short, and it's up to you to make it sweet.",
+  "The key to being happy is knowing you have the power to choose what to accept and what to let go.",
+  "Smile more. Worry less.",
+  "A happy heart makes a cheerful face.",
+  "True happiness is not found in material things but in the love and kindness we share.",
+];
+
+const successQuotes = [
+  "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+  "The way to get started is to quit talking and begin doing.",
+  "Do what you love and success will follow.",
+  "Success is not in what you have, but who you are.",
+  "The road to success and the road to failure are almost exactly the same.",
+  "Success is a journey, not a destination.",
+  "If you want to achieve greatness stop asking for permission.",
+  "Great things never come from comfort zones.",
+  "A little progress each day adds up to big results.",
+  "I never dreamed about success. I worked for it.",
+  "Success is achieved and maintained by those who try and keep trying.",
+  "Your success is only limited by your desire.",
+  "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+  "Success is liking yourself, liking what you do, and liking how you do it.",
+  "To succeed in life, you need two things: ignorance and confidence.",
+];
+
+/* Chom sao */
+
+const nhomChomSao = [
+  (ThienNga = {
+    name: "Thiên nga",
+    description:
+      "Chòm sao Thiên Nga 天鵝, (tiếng La Tinh: Cygnus) là một trong 48 chòm sao Ptolemy và cũng là một trong 88 chòm sao hiện đại, mang hình ảnh con thiên nga.",
+    Represent: "Chòm sao Thiên Nga là đại diện của tình yêu",
+    reasonForRepresentation:
+      "Thiên Nga gắn liền với thần thoại Hy Lạp về tình yêu bất diệt. Zeus hóa thành thiên nga để quyến rũ nữ thần Leda. Ngoài ra, nó còn biểu tượng cho sự thủy chung vì thiên nga thường có bạn đời duy nhất trong suốt cuộc đời.",
+  }),
+  (ThienHau = {
+    name: "Thiên hậu",
+    description:
+      "Chòm sao Thiên Hậu 天后, (tiếng La Tinh: Cassiopeia) là một trong 48 chòm sao Ptolemy và cũng là một trong 88 chòm sao hiện đại, mang hình ảnh nữ thần Cassiopeia.",
+    Represent: "Chòm sao Thiên Hậu là biểu tượng của trí tuệ và sự kiêu hãnh.",
+    reasonForRepresentation:
+      'Trong thiên văn học, chòm sao này có hình dạng chữ "W" hoặc "M", giống như một vương miện, đại diện cho sự hiểu biết và tầm nhìn xa trông rộng.',
+  }),
+  (SuTu = {
+    name: "Sư tử",
+    description:
+      "Sư Tử ( 獅子) (tên Latinh Leo), biểu tượng  là một chòm sao của hoàng đạo, là một trong 48 chòm sao Ptolemy và cũng là một trong 88 chòm sao hiện đại, mang hình ảnh Sư Tử.",
+    Represent:
+      "Chòm sao Sư Tử là biểu tượng của sức mạnh và quyền lực, là nguồn động lực cho bạn ngày hôm nay.",
+    reasonForRepresentation:
+      "Chòm sao Sư Tử đại diện cho sức mạnh, sự kiên trì và lòng dũng cảm. Trong thần thoại, nó liên quan đến con sư tử Nemea mà Hercules phải đánh bại trong 12 chiến công. Đây là biểu tượng của sự không khuất phục trước thử thách.",
+  }),
+  (DaiHung = {
+    name: "Đại hùng",
+    description:
+      "Chòm sao Đại Hùng 大熊, (tiếng La Tinh: Ursa Major), còn được gọi là Gấu Lớn (Great Bear), là một trong 48 chòm sao Ptolemy và cũng là một trong 88 chòm sao hiện đại, mang hình ảnh Gấu Lớn. Chòm sao này là một trong những chòm sao nổi tiếng nhất, không những bởi độ sáng của các ngôi sao thành viên, mà còn bởi huyền thoại Hy Lạp lý thú về chòm sao này. Trong thiên văn học Trung Quốc gọi 7 ngôi sao sáng nhất này là sao Bắc Đẩu.",
+    Represent:
+      "Chòm sao Đại Hùng là biểu tượng của sự mạnh mẽ và kiên định, là lời chúc phúc cho sự thành công của bạn trong hôm nay.",
+    reasonForRepresentation:
+      "Là chòm sao đại diện cho sức mạnh và khả năng dẫn đường (vì chứa sao Bắc Đẩu). Nó biểu tượng cho sự bền bỉ và khả năng định hướng để đạt được thành công.",
+  }),
+  (LapHo = {
+    name: "Lạp hộ",
+    description:
+      "Lạp Hộ (獵戸), nguyên tên gốc là Orion (nhân vật giỏi săn bắn trong thần thoại Hy Lạp), được dịch sang tiếng Hán thành Lạp Hộ, nghĩa là Thợ Săn, là một chòm sao nổi bật, có lẽ được biết nhiều nhất trên bầu trời. Các sao sáng nhất của nó nằm trên xích đạo trời và được quan sát từ khắp mọi nơi trên thế giới, làm cho chòm sao này được biết đến tương đối rộng rãi.",
+    Represent: "Chòm sao Lạp Hộ tượng trưng cho sự vui vẻ, hạnh phúc.",
+    reasonForRepresentation:
+      "Orion là một trong những chòm sao dễ nhận biết nhất trên bầu trời và gắn liền với niềm vui khi quan sát thiên văn. Trong thần thoại, Orion là một thợ săn tài ba, đại diện cho sự tự do và tận hưởng cuộc sống.",
+  }),
+  (MaKet = {
+    name: "Ma kết",
+    description:
+      "Chòm sao Ma Kết (摩羯) hay Nam Dương (南羊) (tiếng La Tinh: Capricornus), biểu tượng  là một trong mười hai chòm sao hoàng đạo, nằm phía tây đối với chòm sao Nhân Mã, phía đông nam đối với chòm sao Bảo Bình, là một trong 48 chòm sao Ptolemy. Chòm sao này còn có tên Ma Kết, Kết Toà, Sơn Dương Tòa, Nam Dương.",
+    Represent:
+      "Chòm sao Ma Kết tượng trưng cho sự kiên trì, cố gắng, với sự chúc phúc của chòm sao này, tôi mong bạn sẽ thành công trong công việc và cuộc sống.",
+    reasonForRepresentation:
+      "Ma Kết là biểu tượng của sự chăm chỉ, kiên nhẫn và quyết tâm. Trong chiêm tinh học, Ma Kết là cung hoàng đạo gắn với sự nghiệp và trách nhiệm, đại diện cho những người luôn nỗ lực để đạt được mục tiêu.",
+  }),
+];
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function start() {
+  const a = localStorage.getItem("Topic");
+  const cauChucSo = localStorage.getItem("Quote");
+  switch (a) {
+    case "1":
+      document.getElementById("cards-constellation").src =
+        "assets/cards/Thiên nga.png";
+      break;
+    case "2":
+      document.getElementById("cards-constellation").src =
+        "assets/cards/Thiên hậu.png";
+      break;
+    case "3":
+      document.getElementById("cards-constellation").src =
+        "assets/cards/Sư tử.png";
+      break;
+    case "4":
+      document.getElementById("cards-constellation").src =
+        "assets/cards/Đại hùng.png";
+      break;
+    case "5":
+      document.getElementById("cards-constellation").src =
+        "assets/cards/Lạp hộ.png";
+      break;
+    case "6":
+      document.getElementById("cards-constellation").src =
+        "assets/cards/Ma kết.png";
+      break;
+  }
+  document.getElementById("avatar").src =
+    avatar[getRandomInt(0, avatar.length - 1)];
+  document.getElementById("header").textContent = nhomChomSao[a - 1].name;
+  document.getElementById("text").textContent =
+    "Ngày hôm nay bạn sẽ được chòm sao " +
+    nhomChomSao[a - 1].name +
+    " chúc phúc.";
+}
+
+document.addEventListener("DOMContentLoaded", start);
